112918ded2266a819dbb065b3e4a190260a2d179
enas-adil <enas307@hotmail.com>
2023-09-15 05:55:39 -0700

    m

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+voi
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}
